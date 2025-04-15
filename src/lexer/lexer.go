306cc9c69d2eca95792098e85be253b7b6bee04c
chimwemwe95 <sammyboywilson@hotmail.co.uk>
2025-04-16 08:17:03 +1000

    weds 16th

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -11,3 +11,17 @@ func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	return l
 }
+
+/*************  ✨ Windsurf Command ⭐  *************/
+// readChar reads the next character from the input and updates the position and
+// readPosition pointers.
+/*******  3420a72d-c324-47c2-a816-60863b9c1737  *******/
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
